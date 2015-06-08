242c99d8efb2524d1e3ac931cd51c4f22de859a1
Michael Aristarkhov <aristarkhov.m@gmail.com>
2015-06-08 21:36:08 +0200

    specs uploaded

--- spec/chatterboxSpec.js
+++ spec/chatterboxSpec.js
@@ -1,3 +1,4 @@
+//specs demonstrate client behavour
 describe('chatterbox', function() {
   it('should parse correctly and have an object named `app`', function(){
     expect(app).to.be.an('object');
